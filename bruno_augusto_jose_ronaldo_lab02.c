e43fe76a2a9be9f9ff4515784d2a42e056b605e1
BACStrange <106183598+BACStrange@users.noreply.github.com>
2022-05-24 15:32:18 -0300

    Update bruno_augusto_jose_ronaldo_lab02.c

--- bruno_augusto_jose_ronaldo_lab02.c
+++ bruno_augusto_jose_ronaldo_lab02.c
@@ -1,3 +1,83 @@
 #include <stdio.h>
-int main(){
+#include <stdlib.h>
+
+typedef char cadeia1[16];
+typedef char cadeia2[21];
+int ngp;
+cadeia1 TabGPs [20];
+
+int LerGPs(){
+/*
+Função LerGPs: função que não retorna nenhum valor, não tem nenhum parâmetro e seu
+objetivo é ler o número de grandes prêmios, guardando-o na variável global ngp, e ler os países
+sedes de todos esses grandes prêmios, guardando-os na variável também global TabGPs.
+*/
+}
+
+int EscreverGPs (){
+/*
+
+*/
+}
+
+int LerPilotos (){
+/*
+
+*/
+}
+
+int LerResultadosGPs (){
+
+/*
+
+*/
+}
+
+int CalcularPontuacao (){
+/*
+
+*/
+}
+
+int OrdenarTabPilotos (){
+/*
+
+*/
+}
+
+int EscreverPilotos (){
+/*
+
+*/
+}
+
+int MontarTabEscuderias (){
+/*
+
+*/
+}
+
+int OrdenarTabEscuderias (){
+/*
+
+*/
+}
+
+int EscreverEscuderias (){
+/*
+
+*/
+}
+
+void main () {
+LerGPs ();
+EscreverGPs ();
+LerPilotos ();
+LerResultadosGPs ();
+CalcularPontuacao ();
+OrdenarTabPilotos ();
+EscreverPilotos ();
+MontarTabEscuderias ();
+OrdenarTabEscuderias ();
+EscreverEscuderias ();
 }
